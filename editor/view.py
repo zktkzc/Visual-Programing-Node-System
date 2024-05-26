b441afaa21d225f0d52bd345e2ebca554f16783b
tkzc00 <2238983030@qq.com>
2024-05-26 16:07:56 +0800

    使用鼠标滚轮放大缩小

--- editor/view.py
+++ editor/view.py
@@ -20,3 +20,22 @@ class View(QGraphicsView):
         # 隐藏滚动条
         self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        # 缩放
+        self._zoom_clamp = [0.5, 5]
+        self._zoom_factor = 1.05
+        self._view_scale = 1.0
+        self.setTransformationAnchor(QGraphicsView.ViewportAnchor.AnchorUnderMouse)
+
+    def wheelEvent(self, event):
+        if event.angleDelta().y() > 0:
+            zoom_factor = self._zoom_factor
+        else:
+            zoom_factor = 1 / self._zoom_factor
+        self._view_scale *= zoom_factor
+        if self._view_scale < self._zoom_clamp[0] or self._view_scale > self._zoom_clamp[1]:
+            zoom_factor = 1.0
+            self._view_scale = self._last_scale
+
+        self._last_scale = self._view_scale
+        # 每一次相对于上一次进行缩放
+        self.scale(zoom_factor, zoom_factor)
