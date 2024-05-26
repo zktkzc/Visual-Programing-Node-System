52141d33114d1cd414076ab71c0a93013a93dd3a
tkzc00 <2238983030@qq.com>
2024-05-26 16:40:27 +0800

    鼠标中键拖动画布

--- editor/view.py
+++ editor/view.py
@@ -2,8 +2,8 @@
 QGraphicsView的子类，是scene的容器
 '''
 
-from PySide6.QtCore import Qt
-from PySide6.QtGui import QPainter
+from PySide6.QtCore import Qt, QEvent
+from PySide6.QtGui import QPainter, QMouseEvent
 from PySide6.QtWidgets import QGraphicsView
 
 import scene
@@ -25,17 +25,67 @@ class View(QGraphicsView):
         self._zoom_factor = 1.05
         self._view_scale = 1.0
         self.setTransformationAnchor(QGraphicsView.ViewportAnchor.AnchorUnderMouse)
+        # 画布拖动
+        self._drag_mode = False
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.MouseButton.MiddleButton:
+            self.middle_button_pressed(event)
+        else:
+            super().mousePressEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.MouseButton.MiddleButton:
+            self.middle_button_released(event)
+        else:
+            super().mouseReleaseEvent(event)
+
+    def mouseDoubleClickEvent(self, event):
+        if event.button() == Qt.MouseButton.MiddleButton:
+            self.reset_scale()
+        else:
+            super().mouseDoubleClickEvent(event)
+
+    def middle_button_pressed(self, event):
+        '''
+        鼠标中间点击
+        :param event:
+        :return:
+        '''
+        if self.itemAt(event.pos()) is not None:
+            return
+        else:
+            # 松开鼠标左键
+            release_event = QMouseEvent(QEvent.Type.MouseButtonRelease, event.localPos(), Qt.MouseButton.LeftButton,
+                                        Qt.MouseButton.NoButton, event.modifiers())
+            super().mouseReleaseEvent(release_event)
+            # 改变鼠标样式为小手
+            self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
+            self._drag_mode = True
+            # 默认为鼠标左键拖动，现在需要同时按住鼠标中建让图标变为小手，太麻烦了，因此创建手动创建一个鼠标左键点击的事件
+            click_event = QMouseEvent(QEvent.Type.MouseButtonPress, event.localPos(), Qt.MouseButton.LeftButton,
+                                      Qt.MouseButton.NoButton, event.modifiers())
+            super().mousePressEvent(click_event)
+
+    def middle_button_released(self, event):
+        self.setDragMode(QGraphicsView.DragMode.NoDrag)
+        self._drag_mode = False
 
     def wheelEvent(self, event):
-        if event.angleDelta().y() > 0:
-            zoom_factor = self._zoom_factor
-        else:
-            zoom_factor = 1 / self._zoom_factor
-        self._view_scale *= zoom_factor
-        if self._view_scale < self._zoom_clamp[0] or self._view_scale > self._zoom_clamp[1]:
-            zoom_factor = 1.0
-            self._view_scale = self._last_scale
+        if not self._drag_mode:
+            if event.angleDelta().y() > 0:
+                zoom_factor = self._zoom_factor
+            else:
+                zoom_factor = 1 / self._zoom_factor
+            self._view_scale *= zoom_factor
+            if self._view_scale < self._zoom_clamp[0] or self._view_scale > self._zoom_clamp[1]:
+                zoom_factor = 1.0
+                self._view_scale = self._last_scale
 
-        self._last_scale = self._view_scale
-        # 每一次相对于上一次进行缩放
-        self.scale(zoom_factor, zoom_factor)
+            self._last_scale = self._view_scale
+            # 每一次相对于上一次进行缩放
+            self.scale(zoom_factor, zoom_factor)
+
+    def reset_scale(self):
+        self.resetTransform()
+        self._view_scale = 1.0
