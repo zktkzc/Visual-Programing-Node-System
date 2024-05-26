068ad14033f585b8a1c38000001fd34c39b41151
tkzc00 <2238983030@qq.com>
2024-05-26 15:51:49 +0800

    画背景网格线

--- editor/view.py
+++ editor/view.py
@@ -2,11 +2,21 @@
 QGraphicsView的子类，是scene的容器
 '''
 
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QPainter
 from PySide6.QtWidgets import QGraphicsView
+
+import scene
 
 
 class View(QGraphicsView):
-    def __init__(self, scene, parent=None):
+    def __init__(self, scene: scene.Scene, parent=None):
         super().__init__(parent)
         self._scene = scene
         self.setScene(self._scene)
+        self.setRenderHints(
+            QPainter.RenderHint.Antialiasing | QPainter.RenderHint.TextAntialiasing | QPainter.RenderHint.SmoothPixmapTransform)
+        self.setViewportUpdateMode(QGraphicsView.ViewportUpdateMode.FullViewportUpdate)
+        # 隐藏滚动条
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
