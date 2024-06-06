28442dd1c311fca764f119b355309ab337349a24
tkzc00 <2238983030@qq.com>
2024-06-06 23:13:28 +0800

    添加菜单栏

--- editorWnd/editor.py
+++ editorWnd/editor.py
@@ -1,16 +1,55 @@
-'''
+"""
 编辑器主体
-'''
+"""
 from __future__ import annotations
 
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QWidget, QBoxLayout
+from PySide6.QtGui import QGuiApplication, QAction
+from PySide6.QtWidgets import QWidget, QBoxLayout, QMainWindow
 
 from editorWnd.env import ENV
 from editorWnd.node import GraphicNode
 from editorWnd.node_port import ParamPort, OutputPort
 from editorWnd.scene import Scene
 from editorWnd.view import View
+
+
+class VisualGraphWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle('可视化编程编辑器')
+        self.resize(1200, 700)
+        editor = Editor(self)
+        self.setCentralWidget(editor)
+        self.__center()
+        # 菜单栏
+        menubar = self.menuBar()
+        file_menu = menubar.addMenu('文件(&F)')
+        self.new_action = QAction(text='&新建', parent=self)
+        self.open_action = QAction(text='&打开', parent=self)
+        self.save_action = QAction(text='&保存', parent=self)
+        file_menu.addActions([self.new_action, self.open_action, self.save_action])
+
+        edit_menu = menubar.addMenu('编辑(&E)')
+        self.copy_action = QAction(text='&复制', parent=self)
+        self.paste_action = QAction(text='&粘贴', parent=self)
+        self.cut_action = QAction(text='&剪切', parent=self)
+        self.undo_action = QAction(text='&撤销', parent=self)
+        self.redo_action = QAction(text='&重做', parent=self)
+        edit_menu.addActions([self.copy_action, self.paste_action, self.cut_action, self.undo_action, self.redo_action])
+
+        selection_menu = menubar.addMenu('选择(&S)')
+
+        run_menu = menubar.addMenu('运行(&R)')
+
+        help_menu = menubar.addMenu('帮助(&H)')
+
+        self.show()
+
+    def __center(self):
+        screen = QGuiApplication.primaryScreen().geometry()
+        size = self.geometry()
+        self.move(int((screen.width() - size.width()) / 2), int((screen.height() - size.height()) / 2))
 
 
 class Editor(QWidget):
@@ -50,11 +89,11 @@ class Editor(QWidget):
         self.view.add_node(node, pos)
 
     def right_click_add_node(self, mouse_pos):
-        '''
+        """
         右键添加节点
         :param mouse_pos: 鼠标点击的位置
         :return:
-        '''
+        """
         self.debug_add_custom_node((mouse_pos.x(), mouse_pos.y()))
 
     def mousePressEvent(self, event):
--- editorWnd/main.py
+++ editorWnd/main.py
@@ -2,11 +2,11 @@ import sys
 import os
 
 from PySide6.QtWidgets import QApplication
-from editorWnd.editor import Editor
+from editorWnd.editor import VisualGraphWindow
 
 if __name__ == '__main__':
     # 把运行目录切换到项目根目录
     os.chdir(os.path.dirname(os.path.dirname(__file__)))
     app = QApplication([])
-    editor = Editor()
+    window = VisualGraphWindow()
     sys.exit(app.exec())
