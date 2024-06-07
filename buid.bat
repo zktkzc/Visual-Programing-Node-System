3ef618834ee4a5e88bef050faec2325bd74119ff
tkzc00 <2238983030@qq.com>
2024-06-07 20:54:38 +0800

    修改打包脚本

--- buid.bat
+++ buid.bat
@@ -2,7 +2,7 @@
 echo 切换环境
 call conda activate python38
 echo 开始执行pyintaller打包命令
-pyinstaller editorWnd\main.py -n 编程可视化编辑器 --add-data editorWnd:editorWnd --add-data assets:assets --add-data 注册ico.bat:. --add-data 取消注册ico.bat:. -y --clean -D -i icon.ico --contents-directory . 
+pyinstaller editorWnd\main.py -n 编程可视化编辑器 --add-data editorWnd:editorWnd --add-data assets:assets --add-data 注册ico.bat:. --add-data 取消注册ico.bat:. -y --clean -D -i assets\icon.ico --contents-directory .
 echo 复制打包后的文件夹
 xcopy /S /Y dist ..
 echo 删除打包创建的临时文件和文件夹
