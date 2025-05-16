24d1956a3155499d079f17af64fb8141a3df174e
tkzc00 <2238983030@qq.com>
2025-05-16 11:36:16 +0800

    更新构建脚本

--- build.bat
+++ build.bat
@@ -1,11 +1,15 @@
 @echo off
+chcp 65001
+echo 切换python环境
+call .venv\Scripts\activate.bat
 echo 开始执行pyintaller打包命令
 pyinstaller editorWnd\main.py -n 编程可视化编辑器 --add-data editorWnd:editorWnd --add-data assets:assets --add-data 注册ico.bat:. --add-data 取消注册ico.bat:. -y --clean -D -i assets\app.ico --contents-directory .
 echo 复制打包后的文件夹
-xcopy /S /Y dist ..
+xcopy /S /Y dist .. > nul
 echo 删除打包创建的临时文件和文件夹
 del /Q *.spec
 rmdir /Q /S dist
 rmdir /Q /S build 2> nul
 timeout /t 1 /nobreak > nul
 rmdir /Q /S build > nul
+pause
