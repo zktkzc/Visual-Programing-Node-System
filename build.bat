573e28d1f4de6d4f7fe5b74a2a4e5c119aaed579
tkzc00 <2238983030@qq.com>
2025-05-16 14:06:30 +0800

    修改构建脚本，给文本添加颜色

--- build.bat
+++ build.bat
@@ -1,15 +1,24 @@
 @echo off
+for /f %%a in ('echo prompt $E^| cmd') do set "ESC=%%a"
 chcp 65001
-echo 切换python环境
+
+echo %ESC%[38;5;42m========================[1.切换python环境]========================%ESC%[0m
 call .venv\Scripts\activate.bat
-echo 开始执行pyintaller打包命令
+
+echo %ESC%[38;5;42m========================[2.开始执行pyintaller打包命令]========================%ESC%[0m
 pyinstaller editorWnd\main.py -n 编程可视化编辑器 --add-data editorWnd:editorWnd --add-data assets:assets --add-data 注册ico.bat:. --add-data 取消注册ico.bat:. -y --clean -D -i assets\app.ico --contents-directory .
-echo 复制打包后的文件夹
-xcopy /S /Y dist .. > nul
-echo 删除打包创建的临时文件和文件夹
+echo %ESC%[38;5;42m================================================================================%ESC%[0m
+
+echo %ESC%[38;5;42m========================[3.复制打包后的文件夹]========================%ESC%[0m
+xcopy /S /Y dist ..
+echo %ESC%[38;5;42m================================================================================%ESC%[0m
+
+echo %ESC%[38;5;42m========================[4.删除打包创建的临时文件和文件夹]========================%ESC%[0m
 del /Q *.spec
 rmdir /Q /S dist
-rmdir /Q /S build 2> nul
+rmdir /Q /S build 2
 timeout /t 1 /nobreak > nul
-rmdir /Q /S build > nul
+rmdir /Q /S build
+echo %ESC%[38;5;42m================================================================================%ESC%[0m
+
 pause
